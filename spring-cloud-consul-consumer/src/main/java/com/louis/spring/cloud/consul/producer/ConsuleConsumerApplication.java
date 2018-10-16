b2ce2e351b990d336a810b34adf1aa8eb4508216
Xu <503018338@logon.ds.ge.com>
2018-10-16 18:18:02 +0800

    consul，支持服务注册和发现

---
+++ spring-cloud-consul-consumer/src/main/java/com/louis/spring/cloud/consul/producer/ConsuleConsumerApplication.java
@@ -0,0 +1,12 @@
+package com.louis.spring.cloud.consul.producer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsuleConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsuleConsumerApplication.class, args);
+	}
+}
