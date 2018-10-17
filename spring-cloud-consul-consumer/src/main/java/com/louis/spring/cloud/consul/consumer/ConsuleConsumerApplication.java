ec6e6a2470d69b34425a5aeef0734d1b17d49919
Xu <503018338@logon.ds.ge.com>
2018-10-17 19:41:46 +0800

    服务消费和负载（Feign）

--- spring-cloud-consul-consumer/src/main/java/com/louis/spring/cloud/consul/consumer/ConsuleConsumerApplication.java
+++ spring-cloud-consul-consumer/src/main/java/com/louis/spring/cloud/consul/consumer/ConsuleConsumerApplication.java
@@ -3,9 +3,11 @@ package com.louis.spring.cloud.consul.consumer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
+@EnableFeignClients
 @SpringBootApplication
 public class ConsuleConsumerApplication {
 
@@ -18,5 +20,4 @@ public class ConsuleConsumerApplication {
 	public RestTemplate restTemplate() {
 		return new RestTemplate();
 	}
-
 }
