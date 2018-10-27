142ec06febf599c27b5bfbebd493bffe51e1739c
Xu <503018338@logon.ds.ge.com>
2018-10-27 13:53:53 +0800

    配置中心（Spring Cloud Config）

---
+++ spring-cloud-conifg-client/src/main/java/com/louis/spring/cloud/config/client/controller/HelloController.java
@@ -0,0 +1,17 @@
+package com.louis.spring.cloud.config.client.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+class HelloController {
+	
+    @Value("${spring.config.hello}")
+    private String hello;
+
+    @RequestMapping("/hello")
+    public String from() {
+        return this.hello;
+    }
+}
